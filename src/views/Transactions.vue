adab5ded2d4fb461df03dd395e63d847ac1a10cf
american38-star <azad.333388@gmail.com>
2025-12-15 12:27:18 +0300

    Update Transactions.vue

--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -101,7 +101,7 @@ export default {
           try {
             const q1 = query(
               collection(db, "transactions"),
-              where("userId", "==", user.uid),
+              where("userId", "==", user.uid), // التأكد من استخدام userId هنا
               orderBy("createdAt", "desc")
             );
             const snap1 = await getDocs(q1);
